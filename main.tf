4809e24d7fa098f59b3f23b218578f5bd3b5e7ff
scott.liao <shazi7804@gmail.com>
2017-10-09 23:21:36 +0800

    Initial commit

---
+++ main.tf
@@ -0,0 +1,18 @@
+# VPC
+resource "aws_vpc" "example" {
+  cidr_block           = "${var.cidr}"
+  instance_tenancy     = "${var.instance_tenancy}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  enable_dns_support   = "${var.enable_dns_support}"
+
+  tags = "${merge(var.tags, map("Name", format("%s", var.name)))}"
+}
+
+# Internet Gateway
+resource "aws_internet_gateway" "example" {
+  count = "${length(var.public_subnets) > 0 ? 1 : 0}"
+
+  vpc_id = "${aws_vpc.this.id}"
+
+  tags = "${merge(var.tags, map("Name", format("%s", var.name)))}"
+}
