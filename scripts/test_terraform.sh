22ddd30afb4cf27ae5f96fb6891c88586303a9d2
scott.liao <shazi7804@gmail.com>
2018-07-27 15:32:57 +0800

    fix terraform command

--- scripts/test_terraform.sh
+++ scripts/test_terraform.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 ./terraform init -input=false
 
-find . -type f -name \"*.tf\" -exec dirname {} \;|sort -u | while read m; do (terraform validate -check-variables=false \"$m\" && echo \"√ $m\") || exit 1 ; done
+find . -type f -name \"*.tf\" -exec dirname {} \;|sort -u | while read m; do (./terraform validate -check-variables=false \"$m\" && echo \"√ $m\") || exit 1 ; done
 
-if [[ -n "$(terraform fmt -write=false)" ]]; then
+if [[ -n "$(./terraform fmt -write=false)" ]]; then
   echo "Some terraform files need be formatted, run 'terraform fmt' to fix";
   exit 1;
 fi
